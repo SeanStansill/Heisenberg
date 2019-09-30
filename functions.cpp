5d87d70f64a89b7b63fb0552514a7a60fe35cc64
SeanStansill <55232829+SeanStansill@users.noreply.github.com>
2019-09-30 10:18:52 +0100

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -7,21 +7,21 @@
 #include <fstream>
 
 
-void get_neighbours(int near_n[N][2]){
-    for (int l = 0; l < N; l++) {
-        near_n[l][0] = ((l + 1) % N);
-        near_n[l][1] = ((l - 1 + N) % N);
+void get_neighbours(int near_n[L][2]){
+    for (int l = 0; l < L; l++) {
+        near_n[l][0] = ((l + 1) % L);
+        near_n[l][1] = ((l - 1 + L) % L);
     }
 }
 
 
-void random_state(double theta[N][N][N], double phi[N][N][N]){
+void random_state(double theta[L][L][L], double phi[L][L][L]){
     std::random_device rd;
     std::mt19937 mt(rd());
     std::uniform_real_distribution<double> ang(0.0, 2.0);
-    for (int l = 0; l < N; l++) {
-        for (int m = 0; m < N; m++) {
-            for (int n = 0; n < N; n++) {
+    for (int l = 0; l < L; l++) {
+        for (int m = 0; m < L; m++) {
+            for (int n = 0; n < L; n++) {
                 double ang1 = ang(mt);
                 double ang2 = ang(mt);
                 theta[l][m][n] = ang1*pi;
@@ -31,10 +31,10 @@ void random_state(double theta[N][N][N], double phi[N][N][N]){
     }
 }
 
-void uniform_state(double theta[N][N][N], double phi[N][N][N]){
-    for (int l = 0; l < N; l++) {
-        for (int m = 0; m < N; m++) {
-            for (int n = 0; n < N; n++) {
+void uniform_state(double theta[L][L][L], double phi[L][L][L]){
+    for (int l = 0; l < L; l++) {
+        for (int m = 0; m < L; m++) {
+            for (int n = 0; n < L; n++) {
                 theta[l][m][n] = 0.0;
                 phi[l][m][n] = pi/2;
             }
@@ -42,15 +42,15 @@ void uniform_state(double theta[N][N][N], double phi[N][N][N]){
     }
 }
 
-std::vector<double> magnetization(double theta[N][N][N], double phi[N][N][N]) {
+std::vector<double> magnetization(double theta[L][L][L], double phi[L][L][L]) {
     double x = 0.0;
     double y = 0.0;
     double z = 0.0;
     std::vector<double> sum;
 
-    for (int l = 0; l < N; ++l) {
-        for (int m = 0; m < N; ++m) {
-            for (int n = 0; n < N; ++n) {
+    for (int l = 0; l < L; ++l) {
+        for (int m = 0; m < L; ++m) {
+            for (int n = 0; n < L; ++n) {
                 x += cos(theta[l][m][n]) * sin(phi[l][m][n]);
                 y += sin(theta[l][m][n]) * sin(phi[l][m][n]);
                 z += cos(phi[l][m][n]);
@@ -58,20 +58,20 @@ std::vector<double> magnetization(double theta[N][N][N], double phi[N][N][N]) {
             }
         }
     }
-    x *= (1/L);
-    y *= (1/L);
-    z *= (1/L);
+    x *= (1/N);
+    y *= (1/N);
+    z *= (1/N);
     sum.push_back(x);
     sum.push_back(y);
     sum.push_back(z);
     return sum;
 }
 
-double total_energy(double theta[N][N][N], double phi[N][N][N], int near_n[N][2]){
+double total_energy(double theta[L][L][L], double phi[L][L][L], int near_n[L][2]){
     double E_t = 0.0;
-    for (int l = 0; l < N; l++) {
-        for (int m = 0; m < N; m++) {
-            for (int n = 0; n < N; n++) {
+    for (int l = 0; l < L; l++) {
+        for (int m = 0; m < L; m++) {
+            for (int n = 0; n < L; n++) {
                 for(int o = 0; o < 2; o++) {
 
                     E_t += (cos(theta[l][m][n]) * sin(phi[l][m][n])) * (cos(theta[near_n[l][o]][m][n]) * sin(phi[near_n[l][o]][m][n]));
@@ -87,11 +87,11 @@ double total_energy(double theta[N][N][N], double phi[N][N][N], int near_n[N][2]
             }
         }
     }
-    E_t *= (1/L)*(-J);
+    E_t *= (1/N)*(-J);
     return E_t;
 }
 
-double local_energy(int i, int j, int k, double theta[N][N][N], double phi[N][N][N], int near_n[N][2]){
+double local_energy(int i, int j, int k, double theta[L][L][L], double phi[L][L][L], int near_n[L][2]){
     double e = 0.0;
     for(int o = 0; o<2; o++) {
         e += cos(theta[i][j][k]) * sin(phi[i][j][k]) * cos(theta[near_n[i][o]][j][k]) * sin(phi[near_n[i][o]][j][k]);
@@ -104,10 +104,10 @@ double local_energy(int i, int j, int k, double theta[N][N][N], double phi[N][N]
         e += cos(phi[i][j][k]) * cos(phi[i][near_n[j][o]][k]);
         e += cos(phi[i][j][k]) * cos(phi[i][j][near_n[k][o]]);
     }
-    e *= (1 / L) * (-J);
+    e *= (1 / N) * (-J);
     return e;
 }
-double new_local_energy(int i, int j, int k, double theta[N][N][N], double phi[N][N][N], int near_n[N][2], double t1[N][N][N] = {}, double t2[N][N][N] = {}){
+double new_local_energy(int i, int j, int k, double theta[L][L][L], double phi[L][L][L], int near_n[L][2], double t1[L][L][L] = {}, double t2[N][N][N] = {}){
     double e = 0.0;
     for(int o = 0; o<2; o++) {
         e += cos(t1[i][j][k]) * sin(t2[i][j][k]) * cos(theta[near_n[i][o]][j][k]) * sin(phi[near_n[i][o]][j][k]);
@@ -120,15 +120,15 @@ double new_local_energy(int i, int j, int k, double theta[N][N][N], double phi[N
         e += cos(t2[i][j][k]) * cos(phi[i][near_n[j][o]][k]);
         e += cos(t2[i][j][k]) * cos(phi[i][j][near_n[k][o]]);
     }
-    e *= (1 / L) * (-J);
+    e *= (1 / N) * (-J);
     return e;
 }
 
-double sum_squares(double theta[N][N][N], double phi[N][N][N], int near_n[N][2]){
+double sum_squares(double theta[L][L][L], double phi[L][L][L], int near_n[L][2]){
     double e_squared = 0.0;
-    for (int l = 0; l < N; l++) {
-        for (int m = 0; m < N; m++) {
-            for (int n = 0; n < N; n++) {
+    for (int l = 0; l < L; l++) {
+        for (int m = 0; m < L; m++) {
+            for (int n = 0; n < L; n++) {
                 for(int o = 0; o < 2; o++) {
                     double E_1 = 0.0;
                     double E_2 = 0.0;
@@ -137,17 +137,17 @@ double sum_squares(double theta[N][N][N], double phi[N][N][N], int near_n[N][2])
                     E_1 += cos(theta[l][m][n]) * sin(phi[l][m][n]) * cos(theta[near_n[l][o]][m][n]) * sin(phi[near_n[l][o]][m][n]);
                     E_1 += (sin(theta[l][m][n]) * sin(phi[l][m][n])) * (sin(theta[near_n[l][o]][m][n]) * sin(phi[near_n[l][o]][m][n]));
                     E_1 += cos(phi[l][m][n]) * cos(phi[near_n[l][o]][m][n]);
-                    E_1 *= (1/L)*(-J);
+                    E_1 *= (1/N)*(-J);
 
                     E_2 += cos(theta[l][m][n]) * sin(phi[l][m][n]) * cos(theta[l][near_n[m][o]][n]) * sin(phi[l][near_n[m][o]][n]);
                     E_2 += (sin(theta[l][m][n]) * sin(phi[l][m][n])) * (sin(theta[l][near_n[m][o]][n]) * sin(phi[l][near_n[m][o]][n]));
                     E_2 += cos(phi[l][m][n]) * cos(phi[l][near_n[m][o]][n]);
-                    E_2 *= (1/L)*(-J);
+                    E_2 *= (1/N)*(-J);
 
                     E_3 += cos(theta[l][m][n]) * sin(phi[l][m][n]) * cos(theta[l][m][near_n[n][o]]) * sin(phi[l][m][near_n[n][o]]);
                     E_3 += (sin(theta[l][m][n]) * sin(phi[l][m][n])) * (sin(theta[l][m][near_n[n][o]]) * sin(phi[l][m][near_n[n][o]]));
                     E_3 += cos(phi[l][m][n]) * cos(phi[l][m][near_n[n][o]]);
-                    E_3 *= (1/L)*(-J);
+                    E_3 *= (1/N)*(-J);
 
                     e_squared += (E_1*E_1) + (E_2*E_2) + (E_3*E_3);
                 }
@@ -157,7 +157,7 @@ double sum_squares(double theta[N][N][N], double phi[N][N][N], int near_n[N][2])
     return e_squared;
 }
 
-double heat_cap(double theta[N][N][N], double phi[N][N][N], int near_n[N][2], double T){  //(double E, double e_squared, double T){
+double heat_cap(double theta[L][L][L], double phi[L][L][L], int near_n[L][2], double T){  //(double E, double e_squared, double T){
     double E = total_energy(theta, phi, near_n);
     double e_squared = sum_squares(theta, phi, near_n);
     double C = (e_squared - (E*E))/(k_B*T*T);
