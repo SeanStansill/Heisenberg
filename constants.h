8e0abe60ff4b5394f34818f26f1d581cb4138216
SeanStansill <55232829+SeanStansill@users.noreply.github.com>
2019-09-30 10:13:24 +0100

    Update constants.h

--- constants.h
+++ constants.h
@@ -3,8 +3,8 @@
 //
 
 const int N = 32;
-const int nsteps = 500;
-const double J = 1.21e-21;
+const int nsteps = 1000;
+const double J = 1.21e-21;  //value for iron so Tc ~ 1000K for reference from https://www.southampton.ac.uk/~rpb/thesis/node18.html
 const double k_B = 1.38e-23;
 const double pi = 3.141592654;
 const double T_init = 10;
