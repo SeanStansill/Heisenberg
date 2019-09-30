95a6d3f441b6f761cdc27d1e55520417a2e8f35f
SeanStansill <55232829+SeanStansill@users.noreply.github.com>
2019-09-30 10:21:36 +0100

    Update functions.h

--- functions.h
+++ functions.h
@@ -2,17 +2,17 @@
 // Created by Sean Stansill [ll14s26s] on 25/09/2019.
 //
 
-void get_neighbours(int near_n[N][2]);
-void uniform_state(double theta[N][N][N], double phi[N][N][N]);
-void random_state(double theta[N][N][N], double phi[N][N][N]);
-std::vector<double> magnetization(double theta[N][N][N], double phi[N][N][N]);
-double total_energy(double theta[N][N][N], double phi[N][N][N], int near_n[N][2]);
-double local_energy(int i, int j, int k, double theta[N][N][N], double phi[N][N][N], int near_n[N][2]);
-double new_local_energy(int i, int j, int k, double theta[N][N][N], double phi[N][N][N], int near_n[N][2], double t1[N][N][N], double t2[N][N][N]);
+void get_neighbours(int near_n[L][2]);
+void uniform_state(double theta[L][L][L], double phi[L][L][L]);
+void random_state(double theta[L][L][L], double phi[L][L][L]);
+std::vector<double> magnetization(double theta[L][L][L], double phi[L][L][L]);
+double total_energy(double theta[L][L][L], double phi[L][L][L], int near_n[L][2]);
+double local_energy(int i, int j, int k, double theta[L][L][L], double phi[L][L][L], int near_n[L][2]);
+double new_local_energy(int i, int j, int k, double theta[L][L][L], double phi[L][L][L], int near_n[L][2], double t1[L][L][L], double t2[L][L][L]);
 void write_M(std::vector<double> M);
 void write_E(double E);
 void write_T(double T);
 void write_C(double C);
-double sum_squares(double theta[N][N][N], double phi[N][N][N], int near_n[N][2]);
+double sum_squares(double theta[L][L][L], double phi[L][L][L], int near_n[L][2]);
 //double heat_cap(double E, double e_squared, double T);
-double heat_cap(double theta[N][N][N], double phi[N][N][N], int near_n[N][2], double T);
+double heat_cap(double theta[L][L][L], double phi[L][L][L], int near_n[L][2], double T);
