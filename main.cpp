e51185d533710829aa3931f395c4f8fb943c37d0
SeanStansill <55232829+SeanStansill@users.noreply.github.com>
2020-02-29 15:42:50 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 
 #include "constants.h"
 #include "functions.h"
+#include <vector>
+#include <cmath>
 #include <omp.h>
+#include <random>
+#include <random>
 #include <iostream>
 
 int main() {
 
-    double theta[N], phi[N], E, T, E_sum, E_sumsquares, C, Mx, My, Mz, M, M_sum, M_sumsquares, M_sumfour, chi, moves_accepted;
+
+    double theta[N], phi[N], E, T = T_init, E_sum, E_sumsquares, C, Mx, My, Mz, M, M_sum, M_sumsquares, M_sumfour, chi, moves_accepted;
     int up[N], down[N], left[N], right[N], backwards[N], forwards[N];
 
     write_N(N);
 
     get_nearest_neighbours(up, down, left, right, backwards, forwards);
 
+    //random_spin_state(theta, phi);
+
+
     int n = omp_get_num_procs();
     omp_set_num_threads(n-1); // n-1 so that when running locally the OS is still snappy and can access a decent amount of power
-#pragma omp parallel default(none) private(theta, phi, E, E_sum, E_sumsquares, Mx, My, Mz, M, M_sum, M_sumsquares, M_sumfour, chi, T, C, moves_accepted, up, down, left, right, backwards, forwards)
+#pragma omp parallel default(none) shared(up, down, left, right, backwards, forwards) private(theta, phi, E, E_sum, E_sumsquares, Mx, My, Mz, M, M_sum, M_sumsquares, M_sumfour, chi, T, C, moves_accepted)
 {
 #pragma omp for schedule(dynamic)
         for (int b = 0; b < T_intervals; b++) {
